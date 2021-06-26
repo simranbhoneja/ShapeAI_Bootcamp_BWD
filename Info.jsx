be5ad303733f1ce5c45bd27fbb69e1c2dcab1de8
Simran Bhoneja <75555922+simranbhoneja@users.noreply.github.com>
2021-06-26 21:03:13 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web dev React Js Bootcamp</p>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+        everything from Scratch inincluding Javascript, React js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
---
+++ index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import List from "./components/List";
+// import Heading from "./components/Heading";
+import App from "./components/App";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
+
+/* <h1 className="hello">Hello</h1>
+    <List />
+    <Heading /> */
+
+// for h1 component create heading.jsx in components folder
+// var name = "Simran";
+/* <h1 class="hello">Hello {name}</h1> */
+
+// IMPORTANT
+// index.js src mein open krnaa h
+// style.css and index.html public mein open krnaa h
+// ul is unordered list
+// li is list item
+// <List></List> aese bhii likh skte h and <List /> aese bhii likh skte h
+// jsx's full form is javascript xml
